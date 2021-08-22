f07f99922982374ae04475909f5b6b5a53f6bfbf
Alejandro Campos Palacios <alcampospalacios@gmail.com>
2021-08-22 14:13:53 -0400

    [ADD] Added design of product-form

--- lib/src/screens/product/product-form-screen.dart
+++ lib/src/screens/product/product-form-screen.dart
@@ -6,16 +6,69 @@ class ProductFormScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _screenSize = MediaQuery.of(context).size;
+
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Stack(
-              children: [ProductFormImageWidget()],
-            )
-          ],
-        ),
+        child: Column(children: [
+          Stack(
+            children: [
+              ProductFormImageWidget(),
+              Positioned(
+                  top: 50,
+                  left: 20,
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.arrow_back_ios_new,
+                      size: 40,
+                      color: Colors.white,
+                    ),
+                    onPressed: () => Navigator.of(context).pop(),
+                  )),
+              Positioned(
+                  top: 50,
+                  right: 20,
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.camera_alt_outlined,
+                      size: 40,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {},
+                  )),
+              Positioned(
+                  top: 300,
+                  left: 0,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(25),
+                        topRight: Radius.circular(25)),
+                    child: Container(
+                      color: Colors.white,
+                      width: _screenSize.width,
+                      height: 200,
+                    ),
+                  ))
+            ],
+          ),
+          _ProductFormReactive()
+        ]),
       ),
+    );
+  }
+}
+
+class _ProductFormReactive extends StatelessWidget {
+  const _ProductFormReactive({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final _screenSize = MediaQuery.of(context).size;
+
+    return Container(
+      color: Colors.white,
+      width: _screenSize.width,
+      height: 400,
     );
   }
 }
--- lib/src/screens/product/product-form-widget.dart
+++ lib/src/screens/product/product-form-widget.dart
@@ -7,32 +7,25 @@ class ProductFormImageWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final _screenSize = MediaQuery.of(context).size;
 
-    return Padding(
-      padding: EdgeInsets.only(left: 10, right: 10, top: 10),
-      child: Container(
-        child: ClipRRect(
+    return Container(
+      child: FadeInImage(
+        placeholder: AssetImage('assets/no-image.png'),
+        image: NetworkImage(
+            'https://cdna.artstation.com/p/assets/images/images/002/074/730/medium/abbey-tex-johnson-abbeytexjohnson-kvothe.jpg?1456885828'),
+        fit: BoxFit.cover,
+      ),
+      width: double.infinity,
+      height: _screenSize.height * 0.4,
+      decoration: BoxDecoration(
+          color: Colors.blue[300],
           borderRadius: BorderRadius.only(
               topLeft: Radius.circular(45), topRight: Radius.circular(45)),
-          child: FadeInImage(
-            placeholder: AssetImage('assets/no-image.png'),
-            image: NetworkImage(
-                'https://cdna.artstation.com/p/assets/images/images/002/074/730/medium/abbey-tex-johnson-abbeytexjohnson-kvothe.jpg?1456885828'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        width: double.infinity,
-        height: _screenSize.height * 0.4,
-        decoration: BoxDecoration(
-            color: Colors.blue[300],
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(45), topRight: Radius.circular(45)),
-            boxShadow: [
-              BoxShadow(
-                  color: Colors.black.withOpacity(0.05),
-                  blurRadius: 10,
-                  offset: Offset(0, 5))
-            ]),
-      ),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black.withOpacity(0.05),
+                blurRadius: 10,
+                offset: Offset(0, 5))
+          ]),
     );
   }
 }
